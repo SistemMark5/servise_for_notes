c571cd7056d3aee1bd30bcc8b4f52e0843d15c16
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-08-28 21:41:39 +0700

    update views endpoints for templates

--- src/views.py
+++ src/views.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, Form
 from sqlalchemy.ext.asyncio import AsyncSession
+from urllib3 import HTTPResponse
 
+from models import Note
 from src.models import db_helper
 from src.models.repository import TaskRepository
-from src.models.dependency import get_by_title
+from src.models.dependency import get_by_title, get_by_id
 from src.models.schemas import CreateNote, AddNote
+from src.utils.template import template
+from fastapi.responses import RedirectResponse
 
 router = APIRouter(
     tags=["Note"],
@@ -14,11 +18,14 @@ router = APIRouter(
 
 @router.post("/create-note")
 async def create_note(
-    note: CreateNote,
+    request: Request,
+    title: str = Form(...),
+    text: str = Form(...),
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
+    note = Note(title=title, text=text)
     await TaskRepository.create_note(note=note, session=session)
-    return {"ok": True}
+    return RedirectResponse(url="/notes", status_code=303)
 
 
 @router.get("/get-note-by-title/{title}")
@@ -26,9 +33,34 @@ async def get_note_by_title(note: AddNote = Depends(get_by_title)):
     return note
 
 
-@router.get("/get-all-notes")
-async def get_all_notes(
+@router.get("/{note_id}/")
+async def get_note_by_id(
+    request: Request,
+    note_id: int,
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
-    response = await TaskRepository.get_all_note(session=session)
-    return response
+    note = await TaskRepository.get_note_for_id(session=session, id=note_id)
+    return template.TemplateResponse(
+        request=request, name="note.html", context={"note": note}
+    )
+
+
+@router.get("/")
+async def get_all_notes(
+    request: Request,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    notes = await TaskRepository.get_all_note(session=session)
+    return template.TemplateResponse(
+        request=request, name="index.html", context={"notes": notes}
+    )
+
+
+@router.delete("/delete/{note_id}")
+async def delete_note(
+    request: Request,
+    note: AddNote = Depends(get_by_id),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    await TaskRepository.delete_note(note=note, session=session)
+    return template.TemplateResponse(request=request, name="index.html")
