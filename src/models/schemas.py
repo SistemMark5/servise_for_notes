7435e52d3adfca48c9d6186f913564f3af46fb6c
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-08-30 21:22:26 +0700

    add ConfigDict

--- src/models/schemas.py
+++ src/models/schemas.py
@@ -13,6 +13,6 @@ class ReadNote(CreateNote):
 class AddNote(CreateNote):
     id: int
 
-    # model_config = ConfigDict(
-    #     from_attributes=True,
-    # )
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
