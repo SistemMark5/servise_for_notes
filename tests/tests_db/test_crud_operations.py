78c93e8b84c1a235903d1dd0ee904344630be651
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-08-30 18:43:03 +0700

    rename test file

---
+++ tests/tests_db/test_crud_operations.py
@@ -0,0 +1,38 @@
+import pytest
+from requests import session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.repository import TaskRepository
+from models.schemas import CreateNote
+
+@pytest.mark.asyncio
+async def test_create_note(session: AsyncSession):
+    async with session:
+        note_data = CreateNote(
+            title="Hello World!",
+            text="Hello World!",
+        )
+        response = await TaskRepository.create_note(session=session, note=note_data)
+        assert response.get("title") == "Hello World!"
+
+@pytest.mark.asyncio
+async def test_get_note_for_title(session: AsyncSession):
+    async with session:
+        title = "Hello World!"
+        response = await TaskRepository.get_note_for_title(session=session, title=title)
+        assert response.title == title
+
+@pytest.mark.asyncio
+async def test_for_get_note_by_id(session: AsyncSession):
+    async with session:
+        note_id = 1
+        response = await TaskRepository.get_note_for_id(session=session, id=note_id)
+        assert response.id is not None and response.id == note_id
+
+@pytest.mark.asyncio
+async def test_delete_note(session: AsyncSession):
+    async with session:
+        note_id = 1
+        note = await TaskRepository.get_note_for_id(session=session, id=note_id)
+        response = await TaskRepository.delete_note(session=session, note=note)
+        assert response is None
