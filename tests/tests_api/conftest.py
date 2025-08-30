441505ffc426a9530b812a69ca9f28f48fd9f4b6
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-08-30 21:24:07 +0700

    create conftest for tests api

---
+++ tests/tests_api/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from main import app
+from models import Note
+from src.models import db_helper
+from models.schemas import CreateNote, AddNote
+
+
+@pytest_asyncio.fixture()
+async def client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test/",
+    ) as client:
+        yield client
+
+@pytest.fixture
+def json_model():
+    return {
+        "title": "Hello World!",
+        "text": "Hello World!"
+    }
+
+@pytest_asyncio.fixture(autouse=True)
+async def override_db_session(session, monkeypatch):
+    async def _get_test_session():
+        return session
+    monkeypatch.setattr(db_helper, "session_dependency", _get_test_session)
+    yield
+
+@pytest_asyncio.fixture
+async def note_factory(session: AsyncSession):
+    async def _create_user(title: str, text: str):
+        note = Note(title=title, text=text)
+        session.add(note)
+        await session.commit()
+        await session.refresh(note)
+        return note
+    return _create_user
