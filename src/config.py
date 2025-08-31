6ffbb43b61f6d4b6fe5dbb026fc933a2cbf4de4c
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-08-31 22:54:42 +0700

    upgrade url for db

--- src/config.py
+++ src/config.py
@@ -10,20 +10,10 @@ class PathTemplates(BaseModel):
     static_path: str = BASE_DIR / "src" / "static"
 
 class DataBaseConfig(BaseModel):
-    MODE: str
-
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def DATABASE_URL_asyncpg(self) -> str:
-        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
+    url: str
+    mode: str
     echo: bool = False
+
 
 
 class Settings(BaseSettings):
