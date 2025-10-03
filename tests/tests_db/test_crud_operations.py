dce0c5b4838709d805942afa64c68498afa24315
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-10-03 09:44:08 +0700

    update test crud operations

--- tests/tests_db/test_crud_operations.py
+++ tests/tests_db/test_crud_operations.py
@@ -2,8 +2,9 @@ import pytest
 from requests import session
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from notes.repository import TaskRepository
+from notes.crud import NoteRepository
 from notes.schemas import CreateNote
+
 
 @pytest.mark.asyncio
 async def test_create_note(session: AsyncSession):
@@ -12,27 +13,30 @@ async def test_create_note(session: AsyncSession):
             title="Hello World!",
             text="Hello World!",
         )
-        response = await TaskRepository.create_note(session=session, note=note_data)
+        response = await NoteRepository.create_note(session=session, note=note_data)
         assert response.get("title") == "Hello World!"
+
 
 @pytest.mark.asyncio
 async def test_get_note_for_title(session: AsyncSession):
     async with session:
         title = "Hello World!"
-        response = await TaskRepository.get_note_for_title(session=session, title=title)
+        response = await NoteRepository.get_note_for_title(session=session, title=title)
         assert response.title == title
+
 
 @pytest.mark.asyncio
 async def test_for_get_note_by_id(session: AsyncSession):
     async with session:
         note_id = 1
-        response = await TaskRepository.get_note_for_id(session=session, id=note_id)
+        response = await NoteRepository.get_note_for_id(session=session, id=note_id)
         assert response.id is not None and response.id == note_id
+
 
 @pytest.mark.asyncio
 async def test_delete_note(session: AsyncSession):
     async with session:
         note_id = 1
-        note = await TaskRepository.get_note_for_id(session=session, id=note_id)
-        response = await TaskRepository.delete_note(session=session, note=note)
+        note = await NoteRepository.get_note_for_id(session=session, id=note_id)
+        response = await NoteRepository.delete_note(session=session, note=note)
         assert response is None
